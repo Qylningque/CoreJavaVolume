85ac3fc65440d6efbd32d0c7bc2a00704524f762
4aBETTERNaN <33279230+4aBETTERNaN@users.noreply.github.com>
2019-04-11 23:34:39 +0800

    设计模式 抽象工厂模式 待完善

---
+++ design/src/top/linco/abstract_factory/Dough.java
@@ -0,0 +1,10 @@
+package src.top.linco.abstract_factory;
+
+/**
+ * 抽象工厂模式
+ */
+public interface Dough {
+
+    public Dough createDough();
+
+}
---
+++ design/src/top/linco/abstract_factory/PizzaIngredientFactory.java
@@ -0,0 +1,9 @@
+package src.top.linco.abstract_factory;
+/**
+ * 抽象工厂模式
+ */
+public interface PizzaIngredientFactory {
+
+    public Dough createDough();
+
+}
