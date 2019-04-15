540d30344350258049724fd55de50b4523706515
Qylningque <qylningque@outlook.com>
2019-04-15 17:46:20 +0800

    Spring容器

---
+++ spring/src/com/linco/chapter_01/knights/config/KnightConfig.java
@@ -0,0 +1,11 @@
+package com.linco.chapter_01.knights.config;
+
+/**
+ * @ClassName KnightConfig
+ * @Description TODO
+ * @Author zbj
+ * @Data 2019/4/15 15:44
+ * @Version 1.0
+ **/
+public class KnightConfig {
+}
